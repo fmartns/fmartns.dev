5da5f84fce5dfc3ba856a4af64469da2cc0e6301
fmartns <hello@fmartns.dev>
2024-04-20 16:37:13 -0300

    Update views.py (Register bug)

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth import login as auth_login, authenticate
 from django.contrib import messages
 from .decorators import anonymous_required
 from dashboard.models import UserProfile
+from django.utils.translation import gettext_lazy as _
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+import re
 
 @anonymous_required
 def login_view(request):
@@ -18,14 +22,22 @@ def login_view(request):
             messages.error(request, 'Invalid username or password.')
             return render(request, 'registration/login.html') 
     return render(request, 'registration/login.html')
+class CustomUserCreationForm(UserCreationForm):
+    def clean_username(self):
+        username = self.cleaned_data.get('username').lower()
+        if not re.match('^[a-z0-9]+$', username):
+            raise forms.ValidationError(_('The username can only contain letters and numbers.'))
+        if len(username) > 30:  # limite de 30 caracteres
+            raise forms.ValidationError(_('The username cannot have more than 30 characters.'))
+        return username
 
 @anonymous_required
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.username = user.username.lower()
+            user.username = form.cleaned_data.get('username')
             user.email = request.POST.get('email', '')
             user.save()
 
@@ -42,5 +54,5 @@ def register(request):
             auth_login(request, user)
             return redirect('dashboard')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
